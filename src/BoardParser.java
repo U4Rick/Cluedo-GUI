3230c3a6d233a42c408a5c701a951a7a3f6aa646
keelyhaskett <49008784+keelyhaskett@users.noreply.github.com>
2020-08-03 14:34:04 +1200

    Created a parser for the board.

---
+++ src/BoardParser.java
@@ -0,0 +1,15 @@
+public class BoardParser {
+
+    private void parseBoard() {
+
+    }
+
+
+    private void parseRoom(String token) {
+
+    }
+
+    private void parseCharacter(String token) {
+
+    }
+}
