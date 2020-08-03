4282c8b281d7f234ba6b23742af438b52476d8f2
keelyhaskett <49008784+keelyhaskett@users.noreply.github.com>
2020-08-03 12:45:23 +1200

    Changed directories.

---
+++ src/Tile.java
@@ -0,0 +1,11 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.30.0.5099.60569f335 modeling language!*/
+
+// line 38 "model.ump"
+public interface Tile
+{
+  
+  // ABSTRACT METHODS 
+
+ public void draw();
+}
---
+++ src/WeaponCard.java
@@ -0,0 +1,29 @@
+/*PLEASE DO NOT EDIT THIS CODE*/
+/*This code was generated using the UMPLE 1.30.0.5099.60569f335 modeling language!*/
+
+
+
+// line 69 "model.ump"
+// line 144 "model.ump"
+public class WeaponCard implements Card
+{
+
+  //------------------------
+  // MEMBER VARIABLES
+  //------------------------
+
+  //------------------------
+  // CONSTRUCTOR
+  //------------------------
+
+  public WeaponCard()
+  {}
+
+  //------------------------
+  // INTERFACE
+  //------------------------
+
+  public void delete()
+  {}
+
+}
