238b9a6bfb9e9acd26d2ee4ac0a0340611cd8568
keelyhaskett <49008784+keelyhaskett@users.noreply.github.com>
2020-08-03 15:51:45 +1200

    Started to build the parser, added initial phases of sorted the cell contents. Added some base functionality to the hallway tiles which will be needed by the parser.

--- src/BoardParser.java
+++ src/BoardParser.java
@@ -1,21 +1,53 @@
-import com.sun.tools.jdeprscan.scan.Scan;
+import Tiles.Tile;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class BoardParser {
 
-    public BoardParser() {}
+    Tile[][] board;
+
+    public BoardParser() {
+        board = new Tile[24][25];
+    }
 
     private void parseBoard(String input) {
         Scanner scan = new Scanner(input);
-        while (scan.hasNext()){
-            String token = scan.next();
+        for (int row = 0; row < board.length; row++) {
+            if (!scan.next().matches("\\|")) { throw new InputMismatchException("Should be a | here"); }
+            for (int col = 0; col < board[row].length; col++) {
+                if (!scan.hasNext()) { throw new NoSuchElementException("Input wrong size"); }
+                String token = scan.next();
+                if (token.matches("~")) {
+                    //inaccessible tile
+                    scan.next();
+                }
+                else if (token.matches("_")) {
+                    //hallway tile
+                    scan.next();
+                }
+                else if (token.matches("[A-Z]")) {
+                    //room entry tile
+                    token = token.concat(scan.next());
+
+                }
+                else if (token.matches("\\|")) {
+                    //room tile
+                    continue;
+                }
+                scan.next();
+            }
         }
     }
 
 
-    private void parseRoom(String token) {
+    private void parseInitials(String token) {
 
+    }
+
+    private void parseRoom(String token) {
+        //TODO: some sort of recursive room tile search function?
     }
 
     private void parseCharacter(String token) {
--- src/Tiles/HallwayTile.java
+++ src/Tiles/HallwayTile.java
@@ -1,6 +1,13 @@
 package Tiles;
 
+import Cards.CharacterCard;
+
 public class HallwayTile extends AccessibleTile {
+
+    private boolean startingTile;
+    private CharacterCard startingCharacter;
+
+    //TODO: add two constructors, one for startingTile true, one for false, true has a characterCard param
 
     public HallwayTile() {
         super();
