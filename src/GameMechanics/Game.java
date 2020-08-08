c38349da9bedb179e4f32c53c5125603159ea1e2
keelyhaskett <49008784+keelyhaskett@users.noreply.github.com>
2020-08-08 15:32:16 +1200

    made sleep().

--- src/GameMechanics/Game.java
+++ src/GameMechanics/Game.java
@@ -308,6 +308,7 @@ public class Game {
                 currentPlayer.madeFalseAccusation();
             }
         }
+        sleep();
     }
 
     /**
@@ -346,11 +347,11 @@ public class Game {
             } else {
                 System.out.println(result.append(refuteWithMultiple(refutableCards)));
             }
-            try { Thread.sleep(2000); } catch (Exception e) { System.out.println(e.toString()); }
+            sleep();
             return true;
         }
         result.append(" cannot refute.");
-        try { Thread.sleep(2000); } catch (Exception e) { System.out.println(e.toString()); }
+        sleep();
         System.out.println(result);
         return false;
     }
@@ -655,7 +656,7 @@ public class Game {
         if (unrefutedSuggestions.isEmpty()) {
             result.append("There are no existing suggestions. \nNo accusation can be made.");
             System.out.println(result);
-            try { Thread.sleep(2000); } catch (Exception e) { System.out.println(e.toString()); }
+            sleep();
             return;
         }
         result.append("Choose one: ");
@@ -698,6 +699,10 @@ public class Game {
     //////////////////////////
     // HELPER METHODS
     //////////////////////////
+
+    private void sleep() {
+        try { Thread.sleep(2000); } catch (Exception e) { System.out.println(e.toString()); }
+    }
 
 
     //////////////////////////
