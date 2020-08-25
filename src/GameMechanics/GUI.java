97411e1bd894efedbfd4f57ee7c9179f0371fd62
keelyhaskett <49008784+keelyhaskett@users.noreply.github.com>
2020-08-25 15:58:24 +1200

    Built basic design of character selection window.

--- src/GameMechanics/GUI.java
+++ src/GameMechanics/GUI.java
@@ -1,6 +1,8 @@
 package GameMechanics;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public abstract class GUI {
 
@@ -11,16 +13,46 @@ public abstract class GUI {
 
     private void buildCharacterSelectWindow() {
         //TODO radio buttons, action listener
-        //TODO submit button, action listener
-        //TODO jlabel title
+        JRadioButton char1 = new JRadioButton("Peacock");
+        JRadioButton char2 = new JRadioButton("Mustard");
+        JRadioButton char3 = new JRadioButton("Scarlett");
+        JRadioButton char4 = new JRadioButton("Plum");
+        JRadioButton char5 = new JRadioButton("White");
+        JRadioButton char6 = new JRadioButton("Green");
 
+        ButtonGroup choices = new ButtonGroup();
+        choices.add(char1);
+        choices.add(char2);
+        choices.add(char3);
+        choices.add(char4);
+        choices.add(char5);
+        choices.add(char6);
+
+
+        JButton submit = new JButton("Submit");
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { buildGameBoard(); }
+        });
+
+        JLabel title = new JLabel("Select Players!");
+
+        JPanel selectPanel = new JPanel();
+        selectPanel.setLayout(new BoxLayout(selectPanel, BoxLayout.PAGE_AXIS));
+        selectPanel.add(title);
+        selectPanel.add(char1);
+        selectPanel.add(char2);
+        selectPanel.add(char3);
+        selectPanel.add(char4);
+        selectPanel.add(char5);
+        selectPanel.add(char6);
+        selectPanel.add(submit);
         selectWindow = new JFrame("Select Players");
 
         selectWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //TODO set layout
-        //TODO set minimum size
+        selectWindow.add(selectPanel);
 
-        //TODO add elements to frame
+        //TODO set minimum size
 
         selectWindow.pack(); //makes all frame contents at or above their preferred size
         selectWindow.setLocationRelativeTo(null); //centers window on screen
@@ -30,15 +62,24 @@ public abstract class GUI {
 
     private void buildGameBoard() {
 
-        //TODO make Jpanels, in split pane?
+        JPanel boardPanel = new JPanel();
+        JPanel optionPanel = new JPanel();
+        JPanel cardPanel = new JPanel();
+
+        //TODO format JPanels.
+
+        JSplitPane innerPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, boardPanel, optionPanel);
+        JSplitPane mainPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, innerPane, cardPanel);
 
         gameWindow = new JFrame();
+        selectWindow.setVisible(false);
 
         gameWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //TODO set layout
         //TODO set minimum size
 
         //TODO add elements to frame
+        gameWindow.add(mainPane);
 
         gameWindow.pack();
         gameWindow.setLocationRelativeTo(null);
